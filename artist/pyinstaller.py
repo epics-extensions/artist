d8d1e50b2463281905a61147bf527588036bc618
Alexis Gaget <alexis.gaget@cea.fr>
2025-02-25 13:51:05 +0100

    add binary wireviz

--- artist/pyinstaller.py
+++ artist/pyinstaller.py
@@ -1,15 +1,25 @@
-import PyInstaller.__main__
+import os
 from pathlib import Path
+
+import PyInstaller.__main__
 
 HERE = Path(__file__).parent.absolute()
 path_to_main = str(HERE / "artist.py")
+def get_wireviz_path():
+    # Obtenir le chemin de l'environnement virtuel
+    env_path = os.getenv('VIRTUAL_ENV')
+    # Construire le chemin complet du binaire wireviz
+    return os.path.join(env_path, 'bin', 'wireviz')
 
 def install():
+    wireviz_path=get_wireviz_path()
+    print(wireviz_path)
     PyInstaller.__main__.run([
         path_to_main,
         '--onefile',
         '--windowed',
         '--hidden-import',
-        'epics.clibs'
-
+        'epics.clibs',
+        f'--add-binary={wireviz_path}:wireviz',
+ 
     ])
