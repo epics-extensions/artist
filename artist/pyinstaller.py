ed14db80d5f43ed267b35264c92913d2b3565b00
Alexis Gaget <alexis.gaget@cea.fr>
2025-02-25 10:28:12 +0100

    change pyinstaller method to hidden import epics.clibs

--- artist/pyinstaller.py
+++ artist/pyinstaller.py
@@ -1,14 +1,19 @@
-import PyInstaller.__main__
+"""Run PyInstaller to create a binary."""
+import subprocess
 from pathlib import Path
 
 HERE = Path(__file__).parent.absolute()
 path_to_main = str(HERE / "artist.py")
 
-def install():
-    PyInstaller.__main__.run([
+def install()->None:
+    """Install Function to build binary with pyInstaller."""
+    command = [
+        "pyinstaller",
         path_to_main,
-        '--onefile',
-        '--windowed',
-        # '--hidden-import epics.clibs',
-        # other pyinstaller options... 
-    ])
+        "--onefile",
+        "--clean",
+        "--hidden-import=epics.clibs",
+        # other pyinstaller options...
+    ]
+    subprocess.run(command, check=True)
+
